df079af80b991f9cb76396f99bce393b285401a4
HaoLin <haolindd@aliyun.com>
2018-10-19 21:01:32 +0800

    创建登录模块并配置路由表

--- src/router.js
+++ src/router.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '@/components/login/login'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: []
+  routes: [
+    {
+      path: '/login',
+      component: Login
+    }
+  ]
 })
