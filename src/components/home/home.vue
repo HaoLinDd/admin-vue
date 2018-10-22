6edb9e07008d0396eb53b8cfdda4ddcf622eee0d
HaoLin <haolindd@aliyun.com>
2018-10-22 11:39:21 +0800

    添加home布局和消息提示

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div>
-    <h1>我是一个主页</h1>
-    <div class="block">
-      <span class="demonstration">区分颜色</span>
-      <el-rate
-        v-model="value2"
-        :colors="['#99A9BF', '#F7BA2A', '#FF9900']">
-      </el-rate>
-      <el-rate
-        v-model="value3"
-        show-text>
-      </el-rate>
-    </div>
-    <router-view></router-view>
+  <div class="container">
+    <el-container class="container">
+      <el-header class="header">Header</el-header>
+      <el-container class="container">
+        <el-aside width="200px" class="aside">Aside</el-aside>
+        <el-main class="main">
+          main
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -34,7 +31,16 @@ export default {
 </script>
 
 <style>
-h1 {
-color: teal;
+.header {
+  background-color: teal;
+}
+.aside {
+  background-color: aliceblue;
+}
+.main {
+  background-color: bisque;
+}
+.container, .main, .aside {
+  height: 100%;
 }
 </style>
--- src/components/login/script.js
+++ src/components/login/script.js
@@ -20,21 +20,32 @@ export default {
     }
   },
   methods: {
-    handelLogin () {
-      axios.post('http://localhost:8888/api/private/v1/login', this.loginForm)
-        .then(res => {
-          const { data, meta } = res.data
-          const { msg, status } = meta
-          window.localStorage.setItem('token', data.token)
-          if (status === 200) {
-            this.$router.push('/')
-          } else if (status === 400) {
-            window.alert(msg)
-          }
-        })
-    },
-    resetForm (formName) {
-      this.$refs['loginFrom'].resetFields()
+    handelLogin (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (!valid) {
+          return false
+        }
+        axios.post('http://localhost:8888/api/private/v1/login', this.loginForm)
+          .then(res => {
+            const { data, meta } = res.data
+            const { msg, status } = meta
+            if (status === 200) {
+              window.localStorage.setItem('token', data.token)
+              this.$router.push('/')
+              this.$message({
+                showClose: true,
+                center: true,
+                type: 'success',
+                message: msg
+              })
+            } else if (status === 400) {
+              this.$message.error({
+                message: msg,
+                center: true
+              })
+            }
+          })
+      })
     }
   }
 }
