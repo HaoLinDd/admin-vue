1ac2e55959901ecc456a32c9aa4c0a426e2408ea
HaoLin <haolindd@aliyun.com>
2018-10-24 15:22:56 +0800

    添加logo

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -4,8 +4,7 @@
       <el-header class="header">
         <el-row>
           <el-col :span="4">
-            logo图标
-            <!-- <img src="./logo.png" alt="黑马程序员"> -->
+            <img src="./element.svg" alt="黑马程序员">
           </el-col>
           <el-col :span="16">电商后台管理系统</el-col>
           <el-col :span="4">
@@ -21,46 +20,46 @@
             :unique-opened="true"
             @open="handleOpen"
             @close="handleClose"
-            background-color="#545c64"
+            background-color="#35495e"
             text-color="#fff"
-            active-text-color="#ffd04b">
+            active-text-color="#41b883">
             <el-submenu index="1">
               <template slot="title">
                 <i class="el-icon-location"></i>
                 <span>用户管理</span>
               </template>
-                <el-menu-item index="1-1">用户列表</el-menu-item>
+              <el-menu-item index="1-1">用户列表</el-menu-item>
             </el-submenu>
             <el-submenu index="2">
               <template slot="title">
                 <i class="el-icon-location"></i>
                 <span>权限管理</span>
-              </template>              
-                <el-menu-item index="2-1">角色列表</el-menu-item>
-                <el-menu-item index="2-2">权限列表</el-menu-item>              
+              </template>
+              <el-menu-item index="2-1">角色列表</el-menu-item>
+              <el-menu-item index="2-2">权限列表</el-menu-item>
             </el-submenu>
             <el-submenu index="3">
               <template slot="title">
                 <i class="el-icon-location"></i>
                 <span>商品管理</span>
-              </template>              
-                <el-menu-item index="3-1">商品列表</el-menu-item>
-                <el-menu-item index="3-2">分类参数</el-menu-item>
-                <el-menu-item index="3-2">商品分类</el-menu-item>             
+              </template>
+              <el-menu-item index="3-1">商品列表</el-menu-item>
+              <el-menu-item index="3-2">分类参数</el-menu-item>
+              <el-menu-item index="3-2">商品分类</el-menu-item>
             </el-submenu>
             <el-submenu index="4">
               <template slot="title">
                 <i class="el-icon-location"></i>
                 <span>订单管理</span>
-              </template>              
-                <el-menu-item index="4-1">订单列表</el-menu-item>
-            </el-submenu>               
+              </template>
+              <el-menu-item index="4-1">订单列表</el-menu-item>
+            </el-submenu>
             <el-submenu index="5">
               <template slot="title">
                 <i class="el-icon-location"></i>
                 <span>数据统计</span>
-              </template>            
-                <el-menu-item index="5-1">数据报表</el-menu-item>         
+              </template>
+              <el-menu-item index="5-1">数据报表</el-menu-item>
             </el-submenu>
           </el-menu>
         </el-aside>
@@ -87,21 +86,33 @@ export default {
     Login
   },
   methods: {
-    handleOpen(key, keyPath) {
+    handleOpen (key, keyPath) {
       console.log(key, keyPath)
     },
-    handleClose(key, keyPath) {
+    handleClose (key, keyPath) {
       console.log(key, keyPath)
     }
   }
-
 }
 </script>
 
 <style>
 .header {
   padding: 0;
-  background-color: #545c64;
+  text-align: center;
+  color: #fff;
+  font-size: 20px;
+  line-height: 60px;
+  background-color: #35495e;
+}
+.header img {
+  vertical-align: middle;
+  display: block;
+  margin: auto;
+  transform: translatey(25%);
+}
+.header a {
+  color: #fff;
 }
 .aside {
   background-color: aliceblue;
